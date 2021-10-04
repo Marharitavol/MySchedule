7b7eadd73bcc760f6b33d1bf97b60cb37b0b04fc
Marharitavol <marharitavol@gmail.com>
2021-10-04 15:25:38 +0300

    - added FSCalendar - create files for vc

---
+++ MySchedule/Controllers/ScheduleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ScheduleViewController.swift
+//  MySchedule
+//
+//  Created by Rita on 04.10.2021.
+//
+
+import UIKit
+
+class ScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        view.backgroundColor = .white
+        title = "Schedule"
+    }
+    
+}
+
---
+++ MySchedule/Controllers/TasksViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TasksViewController.swift
+//  MySchedule
+//
+//  Created by Rita on 04.10.2021.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Tasks"
+    }
+    
+}
